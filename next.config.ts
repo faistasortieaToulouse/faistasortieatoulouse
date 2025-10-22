e756a5ec4a52ea0fbae3f7ee11279ca65fefcd7b
Tolosa-appli <ensemblemidipyrenees@free.fr>
2025-10-22 20:13:46 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from 'next';
+import withPWA from 'next-pwa';
 
-const nextConfig: NextConfig = {
+const nextConfig: NextConfig = withPWA({
   typescript: {
     ignoreBuildErrors: true,
   },
@@ -9,44 +10,20 @@ const nextConfig: NextConfig = {
   },
   images: {
     remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'http',
-        hostname: 'bilingue31.free.fr',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'secure.meetupstatic.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'firebasestorage.googleapis.com',
-        port: '',
-        pathname: '/**',
-      },
+      { protocol: 'https', hostname: 'placehold.co', pathname: '/**' },
+      { protocol: 'https', hostname: 'images.unsplash.com', pathname: '/**' },
+      { protocol: 'https', hostname: 'picsum.photos', pathname: '/**' },
+      { protocol: 'http', hostname: 'bilingue31.free.fr', pathname: '/**' },
+      { protocol: 'https', hostname: 'secure.meetupstatic.com', pathname: '/**' },
+      { protocol: 'https', hostname: 'firebasestorage.googleapis.com', pathname: '/**' },
     ],
   },
-};
+  pwa: {
+    dest: 'public', // dossier où le service worker et manifest seront générés
+    register: true, // enregistre automatiquement le SW
+    skipWaiting: true, // passe au SW suivant dès qu’il est installé
+    disable: process.env.NODE_ENV === 'development', // désactive PWA en dev
+  },
+});
 
 export default nextConfig;
