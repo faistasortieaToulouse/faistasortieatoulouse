cf09613f193734946512dad3b8606bce8d59f0a4
Tolosa-appli <ensemblemidipyrenees@free.fr>
2025-10-22 15:55:28 +0200

    Update discord-widget.tsx

--- src/components/discord-widget.tsx
+++ src/components/discord-widget.tsx
@@ -1,32 +1,23 @@
 'use client';
 
-import { useEffect, useState } from 'react';
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { Card, CardContent, CardHeader, CardTitle, CardDescription } from '@/components/ui/card';
 
 export function DiscordWidget() {
-  const [loaded, setLoaded] = useState(false);
-
-  useEffect(() => {
-    setLoaded(true);
-  }, []);
-
   return (
     <Card>
       <CardHeader>
         <CardTitle>Rejoins la conversation</CardTitle>
       </CardHeader>
       <CardContent>
-        {loaded && (
-          <iframe
-            src="https://discord.com/widget?id=1422806103267344416&theme=dark"
-            width="350"
-            height="500"
-            allowtransparency="true"
-            frameBorder="0"
-            sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"
-            className="rounded-lg"
-          ></iframe>
-        )}
+        <iframe
+          src="https://discord.com/widget?id=1422806103267344416&theme=dark"
+          width="350"
+          height="500"
+          allowtransparency="true"
+          frameBorder="0"
+          sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"
+          className="rounded-lg"
+        ></iframe>
       </CardContent>
     </Card>
   );
