32ee98010b4edab35cb4a3b8bbf60bce9dcbff37
Tolosa-appli <ensemblemidipyrenees@free.fr>
2025-10-22 20:33:37 +0200

    Create next.config.mjs

---
+++ next.config.mjs
@@ -0,0 +1,11 @@
+// next-pwa.config.ts
+import withPWA from 'next-pwa';
+import runtimeCaching from 'next-pwa/cache';
+
+export default withPWA({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
+  runtimeCaching,
+});
