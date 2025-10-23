24935e02536b63cfb87693ed1eb3568d9536958a
Tolosa-appli <ensemblemidipyrenees@free.fr>
2025-10-23 19:09:44 +0200

    Update image-carousel.tsx

--- src/components/image-carousel.tsx
+++ src/components/image-carousel.tsx
@@ -1,10 +1,9 @@
 'use client';
 
 import Image from 'next/image';
+import React from 'react';
 import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from '@/components/ui/carousel';
 import { Card, CardContent } from './ui/card';
-import { CarouselImage } from '@/types/types';
-import React from 'react';
 
 interface Props {
   images: string[]; // juste des URLs maintenant
@@ -14,14 +13,12 @@ export function ImageCarousel({ images }: Props) {
   if (!images || images.length === 0) return <p>Aucune image disponible</p>;
 
   // 🔹 Index de départ aléatoire pour le carrousel
-  const startIndex = React.useMemo(() => {
-    return Math.floor(Math.random() * images.length);
-  }, [images]);
+  const startIndex = React.useMemo(() => Math.floor(Math.random() * images.length), [images]);
 
   return (
     <Carousel
       className="w-full max-w-4xl mx-auto"
-      opts={{ align: 'start', loop: true, startIndex }} // <-- ici
+      opts={{ align: 'start', loop: true, startIndex }}
     >
       <CarouselContent>
         {images.map((imgUrl, index) => (
@@ -31,7 +28,7 @@ export function ImageCarousel({ images }: Props) {
                 <CardContent className="relative flex aspect-video md:aspect-[4/3] lg:aspect-[16/9] items-center justify-center p-0 overflow-hidden rounded-lg">
                   <Image
                     src={imgUrl}
-                    alt={`Image ${index + 1}`} // alt simple basé sur l’index
+                    alt={`Image ${index + 1}`}
                     fill
                     sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
                     style={{ objectFit: 'cover' }}
