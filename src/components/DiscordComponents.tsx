3cc13f80eb64e2fc23a55feba51bef9ff0fc736e
Tolosa-appli <ensemblemidipyrenees@free.fr>
2025-10-23 14:35:19 +0200

    Update DiscordComponents.tsx

--- src/components/DiscordComponents.tsx
+++ src/components/DiscordComponents.tsx
@@ -1,7 +1,7 @@
 // src/components/DiscordComponents.tsx
-export { DiscordStats } from "./DiscordStats";
-export { DiscordWidget } from "./DiscordWidget";
-export { DiscordChannelList } from "./DiscordChannelList";
-export { DiscordEvents } from "./DiscordEvents";
-export { DiscordPolls } from "./DiscordPolls";
-export { AiRecommendations } from "./AiRecommendations";
+export { DiscordStats } from "./Discord-Stats";
+export { DiscordWidget } from "./Discord-Widget";
+export { DiscordChannelList } from "./Discord-Channel-List";
+export { DiscordEvents } from "./Discord-Events";
+export { DiscordPolls } from "./Discord-Polls";
+export { AiRecommendations } from "./Ai-Recommendations";
