9cc5e5809d76edeb9749a79cd90d6afe78c1c1ff
Tolosa-appli <ensemblemidipyrenees@free.fr>
2025-10-24 08:50:24 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,30 @@
-// next-pwa.config.ts
+// next.config.mjs
 import withPWA from 'next-pwa';
 import runtimeCaching from 'next-pwa/cache.js';
 
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    typedRoutes: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
+};
+
 export default withPWA({
-  dest: 'public',
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development',
-  runtimeCaching,
+  ...nextConfig,
+  pwa: {
+    dest: 'public',
+    register: true,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === 'development',
+    runtimeCaching,
+  },
 });
