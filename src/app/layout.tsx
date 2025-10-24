e324fc983f7b319f2066a3d77976c1d0453c8a17
Tolosa-appli <ensemblemidipyrenees@free.fr>
2025-10-24 18:57:45 +0200

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,12 +15,25 @@ export default function RootLayout({
   return (
     <html lang="fr">
       <head>
+        {/* Google Fonts */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
           href="https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap"
           rel="stylesheet"
         />
+
+        {/* Meta PWA / iOS */}
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="theme-color" content="#ffffff" />
+
+        {/* Logo iOS */}
+        <link rel="apple-touch-icon" sizes="180x180" href="/icons/logoFTS180iphone.png" />
+
+        {/* Manifest PWA */}
+        <link rel="manifest" href="/manifest.json" />
       </head>
       <body className="font-body antialiased">
         {children}
