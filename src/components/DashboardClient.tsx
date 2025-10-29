b2713c743acec6bdeb093e205eecb5af47afd267
Tolosa-appli <ensemblemidipyrenees@free.fr>
2025-10-29 19:26:35 +0100

    Update DashboardClient.tsx

--- src/components/DashboardClient.tsx
+++ src/components/DashboardClient.tsx
@@ -98,7 +98,7 @@ export default function DashboardClient({
     <div className="flex flex-col gap-6 w-full">
       {/* Carrousel */}
       <section>
-        <ClientCarousel images={carouselImages} />
+        <ClientCarousel />
       </section>
 
       {/* Stats rapides */}
