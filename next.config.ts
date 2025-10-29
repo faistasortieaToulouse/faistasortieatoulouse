88d1e3ec0439bb373fb05bccb0552918ca720c39
Tolosa-appli <ensemblemidipyrenees@free.fr>
2025-10-29 19:39:43 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,21 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true,
-  typescript: { ignoreBuildErrors: true },
-  eslint: { ignoreDuringBuilds: true },
-  images: {
-    remotePatterns: [
-      { protocol: 'https', hostname: 'placehold.co', pathname: '/**' },
-      { protocol: 'https', hostname: 'images.unsplash.com', pathname: '/**' },
-      { protocol: 'https', hostname: 'picsum.photos', pathname: '/**' },
-      { protocol: 'http', hostname: 'bilingue31.free.fr', pathname: '/**' },
-      { protocol: 'https', hostname: 'secure.meetupstatic.com', pathname: '/**' },
-      { protocol: 'https', hostname: 'firebasestorage.googleapis.com', pathname: '/**' },
-    ],
-  },
+  reactStrictMode: true,
+  typescript: { ignoreBuildErrors: true },
+  eslint: { ignoreDuringBuilds: true },
+  images: {
+    // AJOUT DE CETTE LIGNE POUR DÉSACTIVER L'OPTIMISATION
+    unoptimized: true, 
+    remotePatterns: [
+      { protocol: 'https', hostname: 'placehold.co', pathname: '/**' },
+      { protocol: 'https', hostname: 'images.unsplash.com', pathname: '/**' },
+      { protocol: 'https', hostname: 'picsum.photos', pathname: '/**' },
+      { protocol: 'http', hostname: 'bilingue31.free.fr', pathname: '/**' },
+      { protocol: 'https', hostname: 'secure.meetupstatic.com', pathname: '/**' },
+      { protocol: 'https', hostname: 'firebasestorage.googleapis.com', pathname: '/**' },
+    ],
+  },
 };
 
 export default nextConfig;
