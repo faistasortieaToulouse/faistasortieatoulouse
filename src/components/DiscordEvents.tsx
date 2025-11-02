85e3c0b528caabd3ca54ffaa5db26df7cd2666f6
Tolosa-appli <ensemblemidipyrenees@free.fr>
2025-11-02 07:01:53 +0100

    Update DiscordEvents.tsx

--- src/components/DiscordEvents.tsx
+++ src/components/DiscordEvents.tsx
@@ -34,7 +34,7 @@ const EventImage: React.FC<{ event: DiscordEvent }> = ({ event }) => {
     
     // 4. Rendu de l'image (maintenez les dimensions dans le conteneur parent)
     return (
-        <div className="relative w-full h-16 sm:h-20 md:h-24"> 
+        <div className="relative w-full h-8 sm:h-12 md:h-16">
             <Image
                 src={currentImageSrc}
                 alt={`Image de ${event.name}`}
