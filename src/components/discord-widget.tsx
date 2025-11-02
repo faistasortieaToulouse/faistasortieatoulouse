8f1346b78bec982271b78cfec14ba23b010ad744
Tolosa-appli <ensemblemidipyrenees@free.fr>
2025-11-02 23:09:05 +0100

    Update discord-widget.tsx

--- src/components/discord-widget.tsx
+++ src/components/discord-widget.tsx
@@ -51,7 +51,7 @@ export function DiscordWidget() {
         <iframe
           src="https://discord.com/widget?id=1422806103267344416&theme=dark"
           width="350"
-          height="500"
+          height="900"
           allowTransparency={true}
           frameBorder="0"
           sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"
