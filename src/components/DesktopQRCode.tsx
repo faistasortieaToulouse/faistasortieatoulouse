396aeb3a2c59f2cded8128d759f6cf21b758ca22
Tolosa-appli <ensemblemidipyrenees@free.fr>
2025-11-07 11:04:04 +0100

    Update DesktopQRCode.tsx

--- src/components/DesktopQRCode.tsx
+++ src/components/DesktopQRCode.tsx
@@ -1,11 +1,18 @@
-"use client"
-import QRCode from "react-qr-code";
+"use client";
+
+import { QRCodeCanvas } from "qrcode.react";
 
 export default function DesktopQRCode() {
   return (
     <div className="p-3 bg-white dark:bg-gray-900 rounded-lg shadow-md flex flex-col items-center">
-      <QRCode value="https://faistasortieatoulouse.online" size={170} />
-      <p className="text-xs mt-2 text-gray-600 dark:text-gray-300">Scanner pour ouvrir l'app</p>
+      <QRCodeCanvas
+        value="https://faistasortieatoulouse.online"
+        size={170}
+        includeMargin
+      />
+      <p className="text-xs mt-2 text-gray-600 dark:text-gray-300">
+        Scanner pour ouvrir l'app
+      </p>
     </div>
   );
 }
