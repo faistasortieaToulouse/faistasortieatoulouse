d3b90e3c753ffcbcbf610743d96bf321a4c7a72a
Tolosa-appli <ensemblemidipyrenees@free.fr>
2025-11-09 09:15:26 +0100

    Update DashboardMenu.tsx

--- src/components/DashboardMenu.tsx
+++ src/components/DashboardMenu.tsx
@@ -4,7 +4,7 @@ import { useState } from "react";
 import Link from "next/link";
 import { Download, PartyPopper, Menu, X } from "lucide-react";
 import { Button } from "@/components/ui/button";
-import { AppSidebar } from "@/components/AppSidebar";
+import { AppSidebar } from "@/components/app-sidebar";
 import { StaticImageData } from "next/image";
 
 interface DashboardMenuProps {
